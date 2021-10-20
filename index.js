acc28a23e0cfafc1551a9cfd0262e2180c8c867f
Varun Rai <77796938+kgrai@users.noreply.github.com>
2021-10-20 23:26:25 +0530

    updated

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-$(document).ready(function(){
-    $(".dropdown-menu li a").click(function(){
-    $("#options").text($(this).text());
+$(document).ready(function () {
+    $(".dropdown-menu li a").click(function () {
+        $("#options").text($(this).text());
     });
 });
+function selectbranch() {
+    var e = document.getElementById("branch");
+    let value1 = e.options[e.selectedIndex].value;
+
+    console.log(value1);
+    localStorage.setItem("branch", value1);
+}
+
+function selectsem() {
+    var e = document.getElementById("sem");
+    let value2 = e.options[e.selectedIndex].value;
+
+    console.log(value2);
+    localStorage.setItem("sem", value2);
+}
+function selectscheme() {
+    var e = document.getElementById("scheme");
+    let value3 = e.options[e.selectedIndex].value;
+
+    console.log(value3);
+    localStorage.setItem("scheme", value3);
+}
